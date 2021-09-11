fc246f7ecc095d6d8df688c1b591356566a9d034
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-09-11 23:29:27 +0200

    create Pokemon list
    If applied this will create a list of Pokemon to complete Task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
- 
+let pokemonList = [
+  { name: 'balbasaur', height: 7, type: ['grass', 'poison'] },
+  { name: 'squirtle', height: 6, type: 'water' },
+  { name: 'charmander', height: 5, type: 'fire' },
+];
