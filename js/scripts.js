81ccbab08aa5c18adaf003e5c46a38ff3b8a49d6
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-09-19 12:13:47 +0200

    Task 1.3
    If applied this will create a "for" loop that iterates over each item in pokemonList and a conditional that checks if the height is above a certain value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
-const pokemonList = [
-  { name: 'bulbasaur', height: 7, types: ['grass', 'poison'] },
-  { name: 'squirtle', height: 6, types: ['water'] },
-  { name: 'charmander', height: 5, types: ['fire'] },
+// List of pokemon name + height + types
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Squirtle',
+    height: 6,
+    types: ['water']
+  },
+  {
+    name: 'Charmander',
+    height: 5,
+    types: ['fire']
+  },
 ];
+  // for loop that iterates over each item in pokemonList
+for (let i=0; i < pokemonList.length; i++) {
+  if
+    // if-statement for taller pokemon
+        (pokemonList[i].height >= 7)
+        {document.write('<br>' + pokemonList[i].name + " is " + pokemonList[i].height + " points tall " + "- Wow, that's big!" + '<br>')
+    // different output for pokemon that are smaller than 7 points
+    } else
+    {document.write('<br>' + pokemonList[i].name + " is " + pokemonList[i].height + " points tall." + '<br>')};
+};
