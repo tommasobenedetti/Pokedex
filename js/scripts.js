c8aab452fc99c19bc4822c7805aef4ec98c67cbd
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-09-27 17:40:41 +0200

    Update scripts.js
    Used a pokemon repository variable to hold what my IIFE will return, also modified my loop using the forEach() predefined function to iterate through my pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-// List of pokemon name + height + types
+let pokemonRepository = (function () {
+  //  Create an array within a pokemonRepository called pokemonList,
+  //    and add the pokemon using objects.
 let pokemonList = [
   {
     name: 'Bulbasaur',
@@ -16,13 +18,40 @@ let pokemonList = [
     types: ['fire']
   },
 ];
-  // for loop that iterates over each item in pokemonList
-for (let i=0; i < pokemonList.length; i++) {
-  if
-    // if-statement for taller pokemon
-        (pokemonList[i].height >= 7)
-        {document.write('<br>' + pokemonList[i].name + " is " + pokemonList[i].height + " points tall " + "- Wow, that's big!" + '<br>')
+function getAll() {
+    return pokemonList;
+  }
+  function add(item) {
+    return pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
+// Create a loop using the forEach predifined function to iterate through the array list within the pokemon repository created above. Only rechable by calling the function getAll(); within the line of code.
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let pokemonName = pokemon.name;
+  let pokemonHeight = pokemon.height;
+
+  if (pokemonHeight >= 7) {
+    document.write('<br>' + pokemonName + " is " + pokemonHeight + " points tall " + "- Wow, that's big!" + '<br>');
+    console.log('<br>' + pokemonName + " is " + pokemonHeight + " points tall " + "- Wow, that's big!" + '<br>');
+  } else {
+    document.write(
+      '<br>' + pokemonName + ' is ' + pokemonHeight + ' points tall ' + '<br>'
+    );
+    console.log(
+      '<br>' + pokemonName + ' is ' + pokemonHeight + ' points tall ' + '<br>'
+    );
+  }
+  });
+  //for (let i=0; i < pokemonList.length; i++) {
+  //if
+        //(pokemonList[i].height >= 7)
+        //{document.write('<br>' + pokemonList[i].name + " is " + pokemonList[i].height + " points tall " + "- Wow, that's big!" + '<br>')
     // different output for pokemon that are smaller than 7 points
-    } else
-    {document.write('<br>' + pokemonList[i].name + " is " + pokemonList[i].height + " points tall." + '<br>')};
-};
+    //} else
+    //{document.write('<br>' + pokemonList[i].name + " is " + pokemonList[i].height + " points tall." + '<br>')};
+//};
