980a3dc65ceeb3e32c1e31b2aa44a9528d7f7fe2
tommasobenedetti <tommasobenedetti89@gmail.com>
2021-09-30 14:13:53 +0200

    Update scripts.js
    Used a pokemon repository variable to hold what my IIFE will return, also modified my loop using the forEach() predefined function to iterate through my pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -18,35 +18,47 @@ const pokemonList = [
     types: ['fire']
   },
 ];
+
 function getAll() {
     return pokemonList;
   }
+
   function add(pokemon) {
     return pokemonList.push(pokemon);
   }
 
+  function addListItem(pokemon) {
+    let unOrderedList = document.querySelector('ul');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+
+    button.classList.add('button');
+    listItem.appendChild(button);
+    unOrderedList.appendChild(listItem);
+
+    addEventListener(button, pokemon);
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  function addEventListener(button, pokemon) {
+    button.addEventListener('click', function () {
+      showDetails(pokemon.name);
+    });
+  }
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem,
   };
 })();
-// Create a loop using the forEach predifined function to iterate through the array list within the pokemon repository created above. Only rechable by calling the function getAll(); within the line of code.
-pokemonRepository.getAll().forEach(function (pokemon) {
-  let pokemonName = pokemon.name;
-  let pokemonHeight = pokemon.height;
 
-  if (pokemonHeight >= 7) {
-    document.write('<br>' + pokemonName + " is " + pokemonHeight + " points tall " + "- Wow, that's big!" + '<br>');
-    console.log('<br>' + pokemonName + " is " + pokemonHeight + " points tall " + "- Wow, that's big!" + '<br>');
-  } else {
-    document.write(
-      '<br>' + pokemonName + ' is ' + pokemonHeight + ' points tall ' + '<br>'
-    );
-    console.log(
-      '<br>' + pokemonName + ' is ' + pokemonHeight + ' points tall ' + '<br>'
-    );
-  }
-  });
+  pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
   //for (let i=0; i < pokemonList.length; i++) {
   //if
         //(pokemonList[i].height >= 7)
